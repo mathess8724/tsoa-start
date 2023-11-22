c386ef879e24efe7aa293c4e2d1ca5f2b37f0c2c
Mathieu Moser <moserma@Mathieus-MacBook-Pro.local>
2023-11-22 14:24:35 +0000

    test aws port

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { app } from "./app";
 
 const port = process.env.PORT || 3000;
 const awsport = 5001;
-app.listen(awsport, () =>
-  console.log(`Example app listening at http://localhost:${awsport}`)
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
 );
